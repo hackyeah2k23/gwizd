240f85da9c1b4c683137088eef96ee1171cced5e
semyazza <semyazza@DESKTOP-6T16PUI>
2023-09-30 14:50:14 +0200

    Dodaj pliki projektów.

---
+++ gwizd/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace gwizd
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
