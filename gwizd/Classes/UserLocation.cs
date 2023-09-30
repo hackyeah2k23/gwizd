da287e6c3a5cf7c630d8f3eb8b9be62093592bd4
tomekb530 <tomekb530@gmail.com>
2023-09-30 18:17:17 +0200

    Add Map libraries, and add map to layout

---
+++ gwizd/Classes/UserLocation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gwizd.Classes
+{
+    public class UserLocation
+    {
+        private CancellationTokenSource _cancelTokenSource;
+        private bool _isCheckingLocation;
+
+        public async Task<Location> GetCurrentLocation()
+        {
+            try
+            {
+                _isCheckingLocation = true;
+
+                GeolocationRequest request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+
+                _cancelTokenSource = new CancellationTokenSource();
+
+                Location location = await Geolocation.Default.GetLocationAsync(request, _cancelTokenSource.Token);
+
+                if (location != null)
+                    return location;
+                else
+                    return new Location(0, 0);
+            }
+
+            catch (Exception ex)
+            {
+                return new Location(0, 0);
+            }
+            finally
+            {
+                _isCheckingLocation = false;
+            }
+        }
+
+        public void CancelRequest()
+        {
+            if (_isCheckingLocation && _cancelTokenSource != null && _cancelTokenSource.IsCancellationRequested == false)
+                _cancelTokenSource.Cancel();
+        }
+    }
+}
--- gwizd/MainPage.xaml.cs
+++ gwizd/MainPage.xaml.cs
@@ -1,10 +1,38 @@
-﻿namespace gwizd
+﻿using gwizd.Classes;
+using Microsoft.Maui.Controls.Maps;
+using Microsoft.Maui.Maps;
+
+namespace gwizd
 {
     public partial class MainPage : ContentPage
     {
+        UserLocation locationService;
         public MainPage()
         {
             InitializeComponent();
+            locationService = new UserLocation();
+
+
+            LoadLocation();
         }
+
+        public async void LoadLocation()
+        {
+            Location loc = await locationService.GetCurrentLocation();
+            MapSpan mapSpan = new MapSpan(loc, 0.01, 0.01);
+            mainMap.MoveToRegion(mapSpan);
+            Pin pin = new Pin()
+            {
+                Label = "Jesteś tutaj",
+                Location = loc
+            };
+            mainMap.Pins.Add(pin);
+
+            pin.InfoWindowClicked += (s, args) =>
+            {
+                DisplayAlert("test", "test", "tuguzikhir");
+            };
+        }
+
     }
 }
